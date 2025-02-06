862e75526a89422c54ac0be8cc574de9d90dc16f
Sergionixsex <sergioalbertomr26@gmail.com>
2025-02-06 16:53:59 -0600

    Actividad 3 y diagramas

---
+++ src/main/java/sergio_martinez/actividades/actividad3/Starter.java
@@ -0,0 +1,14 @@
+package sergio_martinez.actividades.actividad3;
+
+import sergio_martinez.actividades.actividad3.ui.CLI;
+
+public class Starter {
+
+    /**
+     * Metodo que se encarga de iniciar la aplicacion
+     */
+    public static void main(String[] args) {
+        CLI.runApp();
+    }
+
+}
